704c6fd97e96a570c23b0dcc581f9aca8b29db1b
Magooch <crvallecito@gmail.com>
2025-02-16 06:06:14 -0700

    Set turn based battle functionality

--- pokemon.py
+++ pokemon.py
@@ -16,28 +16,29 @@ class Pokemon:
     return(f"Pokemon name: {self.name}\n"
            f"Pokemon type: {self.pokemon_type}\n"
            f"Pokemon attack: {self.attack_name}\n"
-           f"Pokemon health: {self.health}")
+           f"Pokemon health: {self.health}\n")
 
   def knockout(self, pokemon):
     pokemon.is_knocked_out = True
     self.health = 0
-    print(f"{pokemon.name} has been knocked out!")
+    print(f"{pokemon.name} has been knocked out!\n")
 
   def attack(self, enemy):
     if (self.pokemon_type == "water" and enemy.pokemon_type == "fire") or (self.pokemon_type == "fire" and enemy.pokemon_type == "plant") or (self.pokemon_type == "electric" and enemy.pokemon_type == "water"):
       if enemy.health <= 25:
         enemy.knockout(enemy)
+        print()
       else:
         new_health = enemy.health - 25
         print(f"{self.name} attacks {enemy.name} with {self.attack_name}")
         print("It is super effective!")
-        print(f"{enemy.name}'s health is now: {new_health}")
+        print(f"{enemy.name}'s health is now: {new_health}\n")
         enemy.health = new_health
     else:
       enemy.health -= 10
       print(f"{self.name} attacks {enemy.name} with {self.attack_name}")
       print("It is not very effective!")
-      print(f"{enemy.name}'s health is now: {enemy.health}")
+      print(f"{enemy.name}'s health is now: {enemy.health}\n")
 
 class Trainer:
   def __init__(self, name):
@@ -49,35 +50,33 @@ class Trainer:
   def revive(self, pokemon):
     if type(pokemon) == Pokemon:
       pokemon.health = 25
-      print(f"{pokemon.name} has been revived with 25 health points!")
+      print(f"{pokemon.name} has been revived with 25 health points!\n")
     else:
-      print("This pokemon is not part of your list!")
+      print("This pokemon is not part of your list!\n")
 
   def potion(self, pokemon):
     if pokemon.is_knocked_out == True:
-      print(f"{pokemon.name} is knocked out!")
+      print(f"{pokemon.name} is knocked out!\n")
     elif pokemon.health > 50:
       diff = 100 - pokemon.health
       pokemon.health = 100
-      print(f"{pokemon.name} has received a {diff}HP increase and is now at full health!")
+      print(f"{pokemon.name} has received a {diff}HP increase and is now at full health!\n")
     else:
       new_health = pokemon.health + 50
       pokemon.health += 50
-      print(f"{pokemon.name} has received a 50HP increase for a new HP level of {new_health}!")
+      print(f"{pokemon.name} has received a 50HP increase for a new HP level of {new_health}!\n")
 
 # Terminal Menu
 while True:
-
   # Trainer One creation
   player_one = input("Player 1 enter your trainer name: ")
   trainer_one = Trainer(player_one)
-  print(f"Hello {trainer_one.name}")
-  input("Press enter to continue...")
   clear_screen() # Clears screen after a choice is made
   # Trainer Two creation
   player_two = input("Player 2 enter your trainer name: ")
   trainer_two = Trainer(player_two)
-  print(f"Hello {trainer_two.name}")
+  clear_screen()
+  print(f"Hello {trainer_one.name} and {trainer_two.name}! Time to choose your Pokemon!")
   input("Press enter to continue...")
   clear_screen() # Clears screen after a choice is made
   
@@ -87,39 +86,78 @@ while True:
   type1 = input("Enter your pokemon's type: ").lower()
   pokemon_one = Pokemon(pokemon1, attack1, type1)
   clear_screen()
-  print(f"{trainer_one.name}'s pokemon stats:\n{pokemon_one}\n")
-  input("Press enter to continue...")
-  clear_screen()
   # Trainer Two pokemon creation
   pokemon2 = input(f"{trainer_two.name} enter your pokemon: ")
   attack2 = input("Enter your pokemon's attack: ")
   type2 = input("Enter your pokemon's type: ").lower()
-  print(f"{trainer_two.name}'s pokemon stats:\n{pokemon2}")
   pokemon_two = Pokemon(pokemon2, attack2, type2)
   clear_screen()
+  print(f"{trainer_one.name}'s pokemon stats:\n{pokemon_one}\n")
+  print()
   print(f"{trainer_two.name}'s pokemon stats:\n{pokemon_two}\n")
-  input("Press enter to continue...")
+  input("Press enter to begin your battle!")
   clear_screen()
 
-  def battle():
-    while True:
-      choice = input(f"press 1 to attack\npress 2 to use a potion\npress 3 to revive\nchoice: ")
-      if choice == '1':
-        clear_screen()
-        pokemon_one.attack(pokemon_two)
-      elif choice == '2':
-        clear_screen()
-        trainer_two.potion(pokemon_two)
-      elif choice == '3':
-        clear_screen()
-        trainer_two.revive(pokemon_two)
-      elif choice == 'exit':
-        print("Exiting stats menu...")
-        clear_screen()
-        break
-      else:
-        clear_screen()
-        print("Invalid choice...")
-      input("Press enter to continue")
+  # Player 1 Battle Menu
+  def p_1_battle():
+    global turn
+    print(f"{trainer_one.name}'s Turn: ")
+    choice = input(f"press 1 to attack\npress 2 to use a potion\npress 3 to revive\npress 4 to show stats\npress 5 to skip your turn\nchoice: ")
+    if choice == '1':
       clear_screen()
-  battle()
+      pokemon_one.attack(pokemon_two)
+      return
+    elif choice == '2':
+      clear_screen()
+      trainer_one.potion(pokemon_one)
+      return
+    elif choice == '3':
+      clear_screen()
+      trainer_one.revive(pokemon_one)
+      return
+    elif choice == '4':
+      clear_screen()
+      print(pokemon_one)
+      return
+    elif choice == '5':
+      print("Press enter to continue")
+      clear_screen()
+      return
+  
+  # Player 2 Battle Menu
+  def p_2_battle():
+    print(f"{trainer_two.name}'s Turn: ")
+    choice = input(f"press 1 to attack\npress 2 to use a potion\npress 3 to revive\npress 4 to show stats\npress 5 to skip your turn\nchoice: ")
+    if choice == '1':
+      clear_screen()
+      pokemon_two.attack(pokemon_one)
+      return
+    elif choice == '2':
+      clear_screen()
+      trainer_two.potion(pokemon_two)
+      return
+    elif choice == '3':
+      clear_screen()
+      trainer_two.revive(pokemon_two)
+      return
+    elif choice == '4':
+      clear_screen()
+      print(pokemon_two)
+      return
+    elif choice == '5':
+      print("Press enter to continue")
+      clear_screen()
+      return
+  
+  # Reset turn count
+  turn = 1
+  # Battle loop (inside main game loop)
+  while True:
+    if pokemon_one.is_knocked_out or pokemon_two.is_knocked_out:
+      break  # End battle if a Pokémon is knocked out
+    if turn % 2 == 1:
+      p_1_battle()
+    else:
+      p_2_battle()
+
+    turn += 1  # Alternate turns
