fb635bf37ef6bbadb3b56c2033c629e265e5d411
Magooch <crvallecito@gmail.com>
2025-02-16 03:25:53 -0700

    Add functionality for revive in battle function

--- pokemon.py
+++ pokemon.py
@@ -5,6 +5,7 @@ def clear_screen():
 
 class Pokemon:
   health = 100
+  is_knocked_out = False
 
   def __init__(self, name, attack_name, pokemon_type):
     self.name = name
@@ -17,15 +18,15 @@ class Pokemon:
            f"Pokemon attack: {self.attack_name}\n"
            f"Pokemon health: {self.health}")
 
-  def knockout(self):
-    is_knocked_out = True
+  def knockout(self, pokemon):
+    pokemon.is_knocked_out = True
     self.health = 0
+    print(f"{pokemon.name} has been knocked out!")
 
   def attack(self, enemy):
     if (self.pokemon_type == "water" and enemy.pokemon_type == "fire") or (self.pokemon_type == "fire" and enemy.pokemon_type == "plant") or (self.pokemon_type == "electric" and enemy.pokemon_type == "water"):
       if enemy.health <= 25:
-        enemy.knockout()
-        print(f"{enemy.name} has been knocked out!")
+        enemy.knockout(enemy)
       else:
         new_health = enemy.health - 25
         print(f"{self.name} attacks {enemy.name} with {self.attack_name}")
@@ -52,15 +53,17 @@ class Trainer:
     else:
       print("This pokemon is not part of your list!")
 
-  def potion(self, pokemon):  
-    if pokemon.health > 50:
+  def potion(self, pokemon):
+    if pokemon.is_knocked_out == True:
+      print(f"{pokemon.name} is knocked out!")
+    elif pokemon.health > 50:
       diff = 100 - pokemon.health
       pokemon.health = 100
       print(f"{pokemon.name} has received a {diff}HP increase and is now at full health!")
-      return
-    new_health = pokemon.health + 50
-    pokemon.health += 50
-    print(f"{pokemon.name} has received a 50HP increase for a new HP level of {new_health}!")
+    else:
+      new_health = pokemon.health + 50
+      pokemon.health += 50
+      print(f"{pokemon.name} has received a 50HP increase for a new HP level of {new_health}!")
 
 # Terminal Menu
 while True:
@@ -100,13 +103,16 @@ while True:
 
   def battle():
     while True:
-      choice = input(f"{trainer_one.name}, press 1 to attack\npress 2 to use a potion")
+      choice = input(f"press 1 to attack\npress 2 to use a potion\npress 3 to revive\nchoice: ")
       if choice == '1':
         clear_screen()
         pokemon_one.attack(pokemon_two)
       elif choice == '2':
         clear_screen()
         trainer_two.potion(pokemon_two)
+      elif choice == '3':
+        clear_screen()
+        trainer_two.revive(pokemon_two)
       elif choice == 'exit':
         print("Exiting stats menu...")
         clear_screen()
